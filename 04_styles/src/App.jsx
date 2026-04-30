8669f5d9f44ae7284bd4d41218e4dff29756cac6
potapchukdmytro <traiget@gmail.com>
2026-04-30 20:55:42 +0300

    mui packages

--- 04_styles/src/App.jsx
+++ 04_styles/src/App.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import { CustomButton } from "./components/buttons/Buttons";
+import {Button, IconButton, Box } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import Rating from '@mui/material/Rating';
 
 function App() {
     return (
         <>
             <Navbar />
 
-            <div>
+            {/* <div>
                 <CustomButton text="Преміум" />
                 <CustomButton text="Залишити відгук" backgroundColor="lightGreen" />
                 <CustomButton text="Оплатити" fontSize="24px" backgroundColor="red" color="white" borderRadius="15px" />
-            </div>
+            </div> */}
+          <Box>
+            <Button variant="contained" color="success">Кнопка MUI</Button>
+            <IconButton aria-label="delete" color="error">
+              <Rating name="read-only" value={3} />
+        <DeleteIcon />
+      </IconButton>
+          </Box>
         </>
     );
 }
