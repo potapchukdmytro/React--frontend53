2f4b597301f1a897e0d5bee9ebd203c3a96928aa
potapchukdmytro <traiget@gmail.com>
2026-05-05 20:28:13 +0300

    App change theme

--- 05_useState/src/App.jsx
+++ 05_useState/src/App.jsx
@@ -1,15 +1,37 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import { useState } from "react";
 import ColoredBox from "./components/coloredBox/ColoredBox";
+import { Button, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const lightTheme = createTheme();
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
 
 function App() {
+    const [isDark, setIsDark] = useState(false);
     console.log("App render");
 
+    const btnThemeText = `Включити ${isDark ? "світлу" : "темну"} тему`;
+
+    function changeTheme() {
+        setIsDark((prev) => !prev);
+    }
+
     return (
-        <>
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+            <CssBaseline />
             <Navbar />
-            <ColoredBox/>
-        </>
+            <ColoredBox />
+            <div style={{ textAlign: "center" }}>
+                <Button color={isDark ? "success" : "info"} onClick={changeTheme} variant="contained">
+                    {btnThemeText}
+                </Button>
+            </div>
+        </ThemeProvider>
     );
 }
 
