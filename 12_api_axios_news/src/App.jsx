e66259a70a6e9ce2917926e720b3a99b5e033eb1
potapchukdmytro <traiget@gmail.com>
2026-05-28 19:04:57 +0300

    12_api_axios_news init

---
+++ 12_api_axios_news/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import {useState, useEffect} from "react";
+import Books from './pages/books/Books'
+import LocalStorageTemplate from './components/localStorage/LocalStorageTemplate';
+import CatsPage from './pages/catsPage/CatsPage';
+import Weather from './pages/weather/Weather';
+
+function App() {
+  // const [counter, setCounter] = useState(0);
+
+  // console.log("Render component");
+
+  // useEffect - хук який виконує код в середині тільки один раз при першому рендері
+  // useEffect(func, dependencyList);
+
+  // useEffect(() => {
+  //   setCounter((prev) => prev + 1)
+  // }, []);
+
+  return (
+    <>
+      {/* <Books/> */}
+      {/* <LocalStorageTemplate/> */}
+      {/* <CatsPage /> */}
+      <Weather/>
+    </>
+  )
+}
+
+export default App
